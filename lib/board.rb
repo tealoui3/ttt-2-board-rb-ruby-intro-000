f2421c3303a03ddb163d188230665279654f1d92
Tiffany Louie <tealouie@gmail.com>
2018-11-01 05:23:26 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,4 @@
 # Define the variable board below.
-row = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
-row[0] = "top left"
-row[1] = "top middle"
-row[2] = "top right"
-row[3] = "middle left"
-row[4] = "middle"
-row[5] = "middle right"
-row[6] = "bottom left"
-row[7] = "bottom middle"
-row[8] = "bottom right"
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board[5] = "X"
+
